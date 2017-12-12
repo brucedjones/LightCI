d06277b528abe57beb460371c6ac416bd5e5dba7
Bruce Jones <bruce.david.jones@gmail.com>
2017-12-12 14:14:48 -0500

    Added specification of configuration file path at launch

--- index.js
+++ index.js
@@ -1,171 +1,18 @@
 #! /usr/bin/env node
 
-const GitHubApi = require('github')
-const async = require('async')
-const clone = require('git-clone');
-const exec = require('child_process').exec;
-const fs = require('fs.extra');
-require('console-stamp')(console, {pattern:'yyyy/mm/dd HH:MM:ss', labelPrefix: '', labelSuffix: ''});
+const light = require('./lightci');
+const program = require('commander');
 
-const conf = require('./light.json')
+program
+  .description('Serverless CI with GitHub')
+  .option('-c, --config </path/to/config>', 'Configuration file')
+  .parse(process.argv);
 
-const github = new GitHubApi();
+var conf = './light.json';
+if(program.config) conf = program.config;
 
-if(!conf.owner) {console.error('No repo owner specified'); return 1;}
-if(!conf.authentication) {console.error('No authentication method specified'); return 1;}
-if(!conf.frequency) {console.warn('No frequency specified, defaulting to 10 minute checking intervals'); conf.frequency = 600000}
-
-github.authenticate(conf.authentication);
-
-var getRepos = function() {
-    return github.repos.getAll({per_page:100}).then(res=>{
-        var repos = res.data
-    
-        var ownedRepos = repos.reduce((prev,curr)=>{
-            if(curr.owner.login == conf.owner) prev.push(curr);
-            return prev;
-        },[])
-
-        return selectReposToWatch(ownedRepos);
-    })
+try {
+    light(require(conf));
+} catch (err) {
+    console.log(err);
 }
-
-var selectReposToWatch = function (ownedRepos){
-    return new Promise((resolve,reject)=>{
-
-        var watchedRepos = [];
-        async.each(ownedRepos,(repo,callback)=>{
-            github.repos.getContent({owner:conf.owner, repo:repo.name, path: ''}).then(res=>{
-                var watchThis = false;
-                res.data.forEach(file=>{
-                    fname = file.name.split('.');
-                    if(fname[0] == 'light' || fname[0] == conf.name && fname[1] == 'sh') watchThis = true;
-                });
-                if(watchThis) watchedRepos.push(repo);
-                callback();
-            }, err=>{
-                var message = JSON.parse(err.message).message;
-                if(message != "This repository is empty.") console.log(err.message)
-                callback();
-            });
-        },(err)=>{
-            if(err) reject(err);
-            else {
-                resolve(watchedRepos);
-            }
-        })
-    })
-}
-
-var checkRepos = function(watchedRepos){
-    return new Promise((resolve,reject)=>{
-        var jobsToDo = [];
-        async.each(watchedRepos,(repo, callback)=>{
-            github.repos.getCommits({owner:conf.owner, repo:repo.name}).then(res=>{
-                var commit = res.data[0];
-                var mostRecentSHA = commit.sha;
-                var assignee = commit.author.login;
-                github.repos.getStatuses({owner:conf.owner, repo:repo.name,sha:mostRecentSHA,ref:mostRecentSHA}).then(res=>{
-                    if(res.data.length == 0) {
-                        github.repos.createStatus({owner:conf.owner, repo:repo.name,sha:mostRecentSHA,ref:mostRecentSHA,state:'pending'})
-                        repo.latestCommit = commit;
-                        jobsToDo.push(repo)
-                    }
-                    callback();
-                });
-            });
-        },err=>{
-            if(err) reject(err);
-            else {
-                resolve(jobsToDo);
-            }
-        });
-    })
-}
-
-var runJobs = function(jobsToDo){
-    jobsToDo.forEach(repo=>{
-        runJob(repo).then(buildResult=>{
-            if(buildResult.success) {
-                github.repos.createStatus({owner:conf.owner, repo:repo.name,sha:repo.latestCommit.sha,ref:repo.latestCommit.sha,state:'success'});
-                closeBuildIssues(repo,repo.latestCommit.sha);
-                console.log('Build succeeded for ' + repo.full_name)
-            }
-            else {
-                github.repos.createStatus({owner:conf.owner, repo:repo.name,sha:repo.latestCommit.sha,ref:repo.latestCommit.sha,state:"failure"});
-                createIssue(repo,repo.latestCommit.sha,buildResult.stdout,buildResult.stderr,repo.latestCommit.author.login);
-                console.log('Build failed for ' + repo.full_name);
-                console.log('stdout:');
-                console.log(buildResult.stdout);
-                console.log('stderr:');
-                console.log(buildResult.stderr);
-            }
-        });
-    });
-}
-
-var createIssue = function(repo,sha,stdout,stderr,assignee){
-
-    var title = "Build test failed for commit "+sha.substring(0,7);
-    var body = "# Build test failed\n Commit " + sha + " failed build testing\n # Output\n ## stdout\n ```\n"+stdout+"``` \n## stderr\n ```\n"+stderr+"```";
-    
-    github.issues.create({owner:conf.owner, repo:repo.name, title:title, body:body, labels:["Build Failed"],assignee:assignee});
-}
-
-var closeBuildIssues = function(repo, fixedBy){
-    github.issues.getForRepo({owner:conf.owner,repo:repo.name,state:"open",labels:"Build Failed"}).then(res=>{
-        var issues = res.data;
-        issues.forEach(issue=>{
-            github.issues.createComment({owner:conf.owner,repo:repo.name,number:issue.number,body: "Resolved with commit " + fixedBy})
-            github.issues.edit({owner:conf.owner,repo:repo.name,number:issue.number, state:"closed"})
-        })
-    });
-}
-
-var runJob = function(repo){
-    return new Promise((resolve,reject)=>{
-        console.log('Running build for ' + repo.full_name)
-
-        var localPath = './work/' + repo.name + '/';
-        fs.rmrfSync(localPath);
-        clone(repo.html_url,localPath,{},(err)=>{
-            if(err) console.log(err)
-
-            exec('sh ' + localPath + 'light.sh', (err, stdout, stderr) => {
-                var success = true;
-                if (err) success=false;
-                
-                fs.rmrfSync(localPath);
-
-                resolve({success: success, stdout:stdout,stderr:stderr});
-              });
-        })
-    });
-}
-
-var checkRateLimit = function(){
-    return new Promise((resolve,reject)=>{
-        github.misc.getRateLimit({}).then(res=>{
-            var remaining = res.data.rate.remaining;
-            var resumeTime = new Date(res.data.rate.reset*1000);
-    
-            console.log(remaining + " GitHub API requests remaining this hour");
-    
-            if(remaining>50) resolve()
-            else reject('GitHub Rate limit exceeded, LightCI will resume at ' + resumeTime);
-        });
-    });
-}
-
-var printError = function(err){log.warn(err)};
-
-var execute = function(){
-    checkRateLimit()
-    .then(getRepos,printError)
-    .then(checkRepos,printError)
-    .then(runJobs,printError);
-    
-    setTimeout(execute,conf.frequency);
-}
-
-execute();
